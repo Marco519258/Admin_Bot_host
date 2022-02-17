83fe1abcb5e99677feeb13430611b02777061bbc
Marco519258 <89468517+Marco519258@users.noreply.github.com>
2022-02-17 20:14:18 +0100

    role claim

---
+++ commands/command/role-claim.js
@@ -0,0 +1,92 @@
+const Discord = require('discord.js')
+const fs = require('fs')
+const reactionRolesConfig = JSON.parse(fs.readFileSync('./data/reactionroles.json','utf8'));
+
+
+module.exports = {
+    name: 'reactionrole',
+    
+    async run (client, message, args) {
+        const channel = '921533454007549963'
+        const Fortniterole = message.guild.roles.cache.find(role => role.id === "921724370328629258")
+        const Minecraftrole = message.guild.roles.cache.find(role => role.name === "Minecraft")
+        const Rainbowrole = message.guild.roles.cache.find(role => role.name === "Rainbow 6 siege")
+        const Valorantrole = message.guild.roles.cache.find(role => role.name === "Valorant")
+        const RocketLeaguerole = message.guild.roles.cache.find(role => role.name === "Rocket League")
+
+        const FortniteEmoji = '1️⃣';
+        const MinecraftEmoji =  '2️⃣';
+        const RainbowEmoji = '3️⃣';
+        const ValoEmoji = '4️⃣';
+        const RocketEmoji = '5️⃣';
+
+        let embed = new Discord.MessageEmbed()
+        .setColor("#002fff")
+        .setTitle('Rollen zuweisung')
+        .setDescription("Wähle deine Spiele um die entsprechende Channel zu sehen.\nFalls es nicht geht, narchicht darüber benutzen\n\n" + `${FortniteEmoji} = ${Fortniterole} \n` + `${MinecraftEmoji} = ${Minecraftrole} \n` + `${RainbowEmoji} = ${Rainbowrole} \n` + `${ValoEmoji} = ${Valorantrole} \n` + `${RocketEmoji} = ${RocketLeaguerole} \n`)
+
+        let messageEmbed = await message.channel.send(embed);
+        messageEmbed.react(FortniteEmoji);
+        messageEmbed.react(MinecraftEmoji);
+        messageEmbed.react(RainbowEmoji);
+        messageEmbed.react(ValoEmoji);
+        messageEmbed.react(RocketEmoji);
+
+        
+
+
+        client.on('messageReactionAdd', async (reaction, user) => {
+            if(reaction.message.partial) await reaction.message.fetch();
+            if(reaction.partial) await reaction.fetch();
+            if(user.bot) return;
+            if(!reaction.message.guild) return;
+
+            if(reaction.message.channel.id == channel) {
+                if(reaction.emoji.name === FortniteEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(Fortniterole)
+                }
+                if(reaction.emoji.name === MinecraftEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(Minecraftrole)
+                }
+                if(reaction.emoji.name === RainbowEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(Rainbowrole)
+                }
+                if(reaction.emoji.name === ValoEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(Valorantrole)
+                }
+                if(reaction.emoji.name === RocketEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(RocketLeaguerole)
+                }
+            } else {
+                return;
+            }
+        });
+
+        client.on('messageReactionRemove', async (reaction, user) => {
+            if(reaction.message.partial) await reaction.message.fetch();
+            if(reaction.partial) await reaction.fetch();
+            if(user.bot) return;
+            if(!reaction.message.guild) return;
+
+            if(reaction.message.channel.id == channel) {
+                if(reaction.emoji.name === FortniteEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(Fortniterole)
+                }
+                if(reaction.emoji.name === MinecraftEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(Minecraftrole)
+                }
+                if(reaction.emoji.name === RainbowEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(Rainbowrole)
+                }
+                if(reaction.emoji.name === ValoEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(Valorantrole)
+                }
+                if(reaction.emoji.name === RocketEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(RocketLeaguerole)
+                }
+            } else {
+                return;
+            }
+        });
+    }
+}
